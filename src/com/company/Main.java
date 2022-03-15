2d5a1330c340f90833c70d5bbac7f1305dfc5009
Chris Luntraru <chris.luntraru@gmail.com>
2022-03-15 11:50:38 +0200

    Changed
    Signed-off-by: Chris Luntraru <chris.luntraru@gmail.com>

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test");
+        System.out.println("Merge Conflict");
     }
 }
